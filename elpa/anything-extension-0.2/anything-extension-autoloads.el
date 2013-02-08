597fd9c5603ba7c8cbd37334f8ba0bceceb47dcf
ncaq <nyrigadake38@gmail.com>
2013-02-08 20:08:39 +0900

    add:ibus remove:migemo change:font,recentf, .gitignore
    ibus.elを追加.前は特に追加しなくてもibusで入力出来てたのに何故だろう.
    しかし,ibus.elの独自機能が便利だから結果オーライ.
    例えば日本語入力時のカラー変更とか便利.
    
    migemo,そもそも自分は名前から日本語排除してるからあんまりいらないし,検索にでしゃばってくるので,コメントアウトしておいた.
    
    fontを10pt→10pxに変更.
    尚,10pxは10.5pt相当.
    pxで偶数にしないと,等幅フォントでも,全角文字が半角文字2つ分にならないことに気がついた.
    それに,こちらのほうがより多くの環境に対応できる.LetinaDisplay欲しいよー…
    
    recentfの領域を10倍に増やす.
    
    emacsのpackage,gitで追うの止めました.非常にログが汚くなる.役割被るし.
    もしこの .emacs.d をcloneして使う人が居たら申し訳ないけど,居ないでしょ…だからよし.
    
    このコミットが遺書になるとはこの時知る由もなかったのであった…

--- elpa/anything-extension-0.2/anything-extension-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; anything-extension-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("anything-extension-pkg.el" "anything-extension.el")
-;;;;;;  (20730 26259 596579))
-
-;;;***
-
-(provide 'anything-extension-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; anything-extension-autoloads.el ends here
--- elpa/el-swank-fuzzy-0.1/el-swank-fuzzy-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; el-swank-fuzzy-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("el-swank-fuzzy-pkg.el" "el-swank-fuzzy.el")
-;;;;;;  (20730 26294 245475))
-
-;;;***
-
-(provide 'el-swank-fuzzy-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; el-swank-fuzzy-autoloads.el ends here
