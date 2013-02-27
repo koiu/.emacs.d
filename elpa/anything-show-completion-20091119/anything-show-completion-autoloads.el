49cb732c76f6138943c40253958334d2b6fca722
ncaq <nyrigadake38@gmail.com>
2013-02-27 20:29:06 +0900

    add:emacs package system in git repository
    やっぱりelpaもgit管理下に置くことに.
    ただしmarmaladeのパッケージ一覧ファイルは改行がないので変更が毎回ひどいことになる.
    故に無視する.
    
    あと, .emacs.d の外に依存するものをREADME.mdに書いておいた.

---
+++ elpa/anything-show-completion-20091119/anything-show-completion-autoloads.el
@@ -0,0 +1,18 @@
+;;; anything-show-completion-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("anything-show-completion-pkg.el" "anything-show-completion.el")
+;;;;;;  (20730 26252 351249))
+
+;;;***
+
+(provide 'anything-show-completion-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; anything-show-completion-autoloads.el ends here
